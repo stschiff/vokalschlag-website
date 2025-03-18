f81396908af1b645e73e6968a3d9d1db4ed86ee3
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2025-03-18 15:38:05 +0100

    fixed ensemble layout

--- src/Ensemble.vue
+++ src/Ensemble.vue
@@ -25,7 +25,7 @@ export default {
 <template>
     <section class="container has-background-grey-lighter has-text-weight-light p-6">
         <h2 class="is-size-3 has-text-weight-light mb-5" id="besetzung">Ensemblemitglieder</h2>
-        <div class="fixed-grid has-3-cols has-text-centered">
+        <div class="fixed-grid has-3-cols-tablet has-2-cols-mobile has-text-centered">
             <div class="grid">
                 <div class='cell m-3' v-for="singer in singers">
                     <figure class="image">
